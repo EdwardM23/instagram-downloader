c8118047878c3145a302bec9d1f0279b8698e81b
Christopher Edward <scrith.edward23@gmail.com>
2024-03-14 18:45:40 +0700

    Initial commit

---
+++ app/src/main/java/com/edward/instagramdownloader/ui/home/HomeFragment.kt
@@ -0,0 +1,45 @@
+package com.edward.instagramdownloader.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.edward.instagramdownloader.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val input_url: EditText = binding.inputUrl
+        val button_search: ImageButton = binding.buttonSearch
+
+        button_search.setOnClickListener {
+            Toast.makeText(context, "test", Toast.LENGTH_SHORT).show()
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
